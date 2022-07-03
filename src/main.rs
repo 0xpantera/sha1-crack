432b3792c74ef53608d968d5e5644405b537f839
Francososa <franky.sosa@gmail.com>
2022-07-02 19:28:22 -0600

    Setup basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    error::Error,
+};
+
+const SHA1_HEX_STRING_LENGTH: usize = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return Ok(());
+    }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    Ok(())
 }
