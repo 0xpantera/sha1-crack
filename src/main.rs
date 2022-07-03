5c85556639c2ecedb2e875faf0c0b60b59da9cfc
Francososa <franky.sosa@gmail.com>
2022-07-02 19:39:22 -0600

    Continue setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{
     env,
     error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
 };
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
@@ -17,6 +19,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let hash_to_crack = args[2].trim();
     if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
         return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
     }
 
     Ok(())
